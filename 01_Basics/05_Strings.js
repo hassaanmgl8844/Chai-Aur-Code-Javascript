2855bf9c22faa3b76de0b5dc57002f5b0c495551
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-03 19:08:05 +0500

    Strings In Javascript

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,8 @@
+const name = "hassaan";
+const repoCount = "12";
+
+// console.log(name + " repo count: " + repoCount);
+
+// String Interpolation
+console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
