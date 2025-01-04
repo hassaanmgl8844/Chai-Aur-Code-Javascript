eeff75b74f1404c949adf966b43e735f0d547c61
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-04 17:51:50 +0500

    Practise String

--- 01_Basics/05_Strings.js
+++ 01_Basics/05_Strings.js
@@ -2,17 +2,17 @@ const name = "hassaan";
 
 const repoCount = 50;
 
-// console.log(name + " repo count: " + repoCount);
+console.log(name + " repo count: " + repoCount);
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String("hassaan");
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
 
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('a'));
